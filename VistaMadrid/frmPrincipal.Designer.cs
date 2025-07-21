912e7102caad792fb2ff2d7fe7d13d4bdcdfae58
Randi Morel <70826138+Randi-Morel@users.noreply.github.com>
2025-07-20 20:56:05 -0400

    Rename Form1 to frmPrincipal

--- VistaMadrid/frmPrincipal.Designer.cs
+++ VistaMadrid/frmPrincipal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace VistaMadrid
 {
-    partial class Form1
+    partial class frmPrincipal
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -31,7 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "frmPrincipal";
         }
 
         #endregion
