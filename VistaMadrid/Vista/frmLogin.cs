4337227f5b1e25aa1bbcbb3b6f4bc48bc0903803
Jeann1008 <jeanperez0810@gmail.com>
2025-07-24 11:55:27 -0400

    El form login todavia no tiene boton para cerrar y minimizar. Los textbox's del login aparcen de otro tamaño en la vista de diseño, pero es normal, no recomiendo tocar nada, ya que en el initialize se pone el tamaño que deberia. Los textbox's tienen debajo un panel que simula el borde. En el program.cs hay 4 lineas de codigo en total, que sirven para que el programa se vea a buena resolucion.

---
+++ VistaMadrid/Vista/frmLogin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VistaMadrid
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+
+            //Sirve para añadirle altura customizada
+            this.txtUsuario.AutoSize = false;
+            this.txtUsuario.Size = new System.Drawing.Size(309, 32);
+
+            this.txtPassword.AutoSize = false;
+            this.txtPassword.Size = new System.Drawing.Size(309, 32);
+        }
+
+      
+    }
+}
