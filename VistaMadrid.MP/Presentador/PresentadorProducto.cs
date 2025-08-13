7ef4765f08f5c5865a2b8e4c3fc7560c1c9407d5
Randi Morel <randimora555@gmail.com>
2025-08-13 19:18:28 -0400

    cosas

---
+++ VistaMadrid.MP/Presentador/PresentadorProducto.cs
@@ -0,0 +1,15 @@
+﻿using VistaMadrid.MP.EF;
+namespace VistaMadrid.MP
+
+{
+    public class PresentadorProducto
+    {
+        private readonly IProducto _view;
+
+        public PresentadorProducto(IProducto view)
+        {
+            _view = view;
+        }
+
+    }
+}
---
+++ VistaMadrid.MP/Vista/Producto/Interfaces/IProducto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace VistaMadrid.MP
+{
+    public interface IProducto
+    {
+        string Usuario { get; set; }
+        string Password { get; set; }
+
+        PresentadorLogin Presentador { get; set; }
+    }
+}
